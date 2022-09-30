593ef8d60ef53e0617da868efc0a0e8130b2b1ad
icastro09 <icastro@akurey.com>
2022-09-30 10:12:13 -0600

    Implementation of faker-js/faker

--- src/tests/pets/addNewPet.test.ts
+++ src/tests/pets/addNewPet.test.ts
@@ -1,10 +1,12 @@
 import { addNewPet } from '@services/pets';
 import { addPetSchema } from '@schemas/pets';
 import { schemaValidator } from '@schemas/pets/index';
+import { faker } from '@faker-js/faker';
+
 
 describe('should add new pet', () => {
   it('with just pet name', async () => {
-    const petName = 'new pet';
+    const petName = faker.name.firstName();
     const { data, status } = await addNewPet(petName);
     expect(status).toEqual(200);
     expect(schemaValidator(addPetSchema(), data)).toEqual(true);
