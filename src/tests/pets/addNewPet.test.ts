0860532734a474131fdc60264c28f3f031792d7c
nahomymoya-ak <nmoya@akurey.com>
2023-01-02 15:19:53 -0600

    Actions: test fix

--- src/tests/pets/addNewPet.test.ts
+++ src/tests/pets/addNewPet.test.ts
@@ -7,7 +7,6 @@ describe("should add new pet", () => {
   it("with just pet name", async () => {
     const petName = faker.name.firstName();
     const { data, status } = await addNewPet(petName);
-    console.log(data);
     expect(status).toEqual(200);
     expect(schemaValidator(addPetSchema(), data)).toEqual(true);
     expect(data.name).toEqual(petName);
