e326f1f1487aee34f1622909ca9c63d554909fdf
nahomymoya-ak <nmoya@akurey.com>
2023-01-02 13:56:13 -0600

    Actions: test

--- src/tests/pets/addNewPet.test.ts
+++ src/tests/pets/addNewPet.test.ts
@@ -1,11 +1,11 @@
-import { addNewPet } from '@services/pets';
-import { addPetSchema } from '@schemas/pets';
-import { schemaValidator } from '@schemas/pets/index';
-import { faker } from '@faker-js/faker';
+import { addNewPet } from "@services/pets";
+import { addPetSchema } from "@schemas/pets";
+import { schemaValidator } from "@schemas/pets/index";
+import { faker } from "@faker-js/faker";
 
-
-describe('should add new pet', () => {
-  it('with just pet name', async () => {
+describe("should add new pet", () => {
+  it("with just pet name", async () => {
+    console.log("holis");
     const petName = faker.name.firstName();
     const { data, status } = await addNewPet(petName);
     expect(status).toEqual(200);
