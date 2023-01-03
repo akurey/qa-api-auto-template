6ca3f781193e378790d7efcdafabc458c3dcd393
nahomymoya-ak <nmoya@akurey.com>
2023-01-03 08:40:27 -0600

    Actions: addition of linters

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    overrides: [
+      {
+        files: '*.{js,ts}',
+        rules: { '@typescript-eslint/no-var-requires': 'off' },
+      },
+    ],
+    rules: {
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    },
+  };
